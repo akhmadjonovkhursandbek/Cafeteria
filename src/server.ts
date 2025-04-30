56dcc8108f71fc3ade73d87e79e6277041df9b9e
Token <axmadjonovxursandbek7@gmail.com>
2025-05-01 01:50:54 +0900

    feat: name is changed to Cafeteria from zero

--- src/server.ts
+++ src/server.ts
@@ -4,20 +4,21 @@
 
 // Design pattern: Middleware, Decotar    == malum bir suyak (qo'l masalan)
 
-
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 import app from "./app";
 
+mongoose.set("strictQuery", true);
+
 mongoose
-.connect(process.env.MONGO_URL as string, {})
-.then((data) => {
-   console.log('MongoDB connection succeed');
-   const PORT = process.env.PORT ?? 3003;
-   app.listen(PORT, function() {
-    console.info(`The server is running successfully on port: ${PORT}`);
-    console.info(`Admin project on http://localhost:${PORT}/admin \n`);
-   });
-})
-.catch((err) => console.log("ERROR on connection MongoDB", err));
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("MongoDB connection succeed");
+    const PORT = process.env.PORT ?? 3003;
+    app.listen(PORT, function () {
+      console.info(`The server is running successfully on port: ${PORT}`);
+      console.info(`Admin project on http://localhost:${PORT}/admin \n`);
+    });
+  })
+  .catch((err) => console.log("ERROR on connection MongoDB", err));
